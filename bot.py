0bc9f1ab59e8638bb27f89c3cb2529649dc65bdb
VVAIZU <foxtacomaster@gmail.com>
2023-11-16 22:33:24 +0300

    1

--- bot.py
+++ bot.py
@@ -32,7 +32,6 @@ def send_welcome(message):
 
 @bot.message_handler(content_types="web_app_data") #получаем отправленные данные 
 def answer(webAppMes):
-   print(webAppMes) #вся информация о сообщении
    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
    bot.send_message(webAppMes.chat.id, f"Спасибо, запрос о записи отправлен менеджеру. Вы выбрали: {webAppMes.web_app_data.data}") 
    #отправляем сообщение в ответ на отправку данных из веб-приложения 
