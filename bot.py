777b523fd15cce7cbc581855dfff7e97f27461e0
SergeyStavrovsk <41266421+VVAIZU@users.noreply.github.com>
2024-07-11 18:45:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,22 +124,6 @@ def handle_text_message(message):
         print(e)
         bot.send_message(message.chat.id, "Произошла ошибка при обработке вашего сообщения")
 
-
-# # Callback для выбора салона
-# @bot.callback_query_handler(lambda call: call.data.startswith('choose_shop_yes_') or call.data.startswith('choose_shop_no_'))
-# def handle_choose_shop_query(call):
-#     user_id = call.from_user.id
-#     action, shop_id = call.data.split('_')[2], call.data.split('_')[3]
-
-#     if action == 'yes':
-#         # Обработка случая, когда пользователь хочет выбрать салон
-#         cursor.execute("UPDATE users SET selected_shop_id = %s WHERE telegram_id = %s", (shop_id, user_id))
-#         conn.commit()
-#         bot.send_message(call.message.chat.id, "Салон успешно выбран! Используйте конструктор, чтобы изменить или создать новую запись!", reply_markup=webAppKeyboard())
-
-#     elif action == 'no':
-#         bot.send_message(call.message.chat.id, "Ок, вы можете выбрать салон позже.")
-
 # Callback для выбора салона
 @bot.callback_query_handler(lambda call: call.data.startswith('choose_shop_yes_') or call.data.startswith('choose_shop_no_'))
 def handle_choose_shop_query(call):
@@ -173,18 +157,6 @@ def handle_choose_shop_query(call):
     elif action == 'no':
         bot.send_message(call.message.chat.id, "Ок, вы можете выбрать салон позже.")
 
-# # Функция для обработки введенного номера телефона
-# def handle_phone_input(message):
-#     user_id = message.from_user.id
-#     phone_number = message.text
-
-#     # Здесь вы можете добавить дополнительную валидацию номера телефона
-
-#     # Обновляем номер телефона в базе данных пользователя
-#     cursor.execute("UPDATE users SET phone_number = %s WHERE telegram_id = %s", (phone_number, user_id))
-#     conn.commit()
-
-#     bot.send_message(message.chat.id, f"Спасибо! Номер телефона {phone_number} успешно сохранен. Используйте конструктор, чтобы изменить или создать новую запись!", reply_markup=webAppKeyboard())
 
 def handle_phone_input(message):
     user_id = message.from_user.id
@@ -244,115 +216,3 @@ def answer(webAppMes):
 if __name__ == '__main__':
     bot.infinity_polling()
 
-
-# # CALLBACKQUERY FROM KEYBOARD
-# @bot.callback_query_handler(lambda call: call.data.startswith('shop_'))
-# def handle_shop_query(call):
-#     shop_id = int(call.data.split('_')[1])
-#     user_id = call.from_user.id
-
-#     # Проверяем, есть ли пользователь в базе
-#     cursor.execute("SELECT id FROM users WHERE telegram_id = %s", (user_id,))
-#     existing_user = cursor.fetchone()
-#     if existing_user:
-#             # Если пользователь уже существует, обновляем выбранный магазин
-#             cursor.execute("UPDATE users SET selected_shop_id = %s WHERE telegram_id = %s", (shop_id, user_id))
-#     else:
-#         # Если пользователь не существует, добавляем его в базу
-#         cursor.execute("INSERT INTO users (telegram_id, selected_shop_id) VALUES (%s, %s)", (user_id, shop_id))
-
-#     conn.commit()
-
-#     #Запрос к PostgreSQL для получения данных о выбранном салоне
-#     cursor.execute("SELECT name, info, image_url FROM shops WHERE id = %s", (shop_id,))
-#     shop_data = cursor.fetchone()
-
-#     if shop_data:
-#         shop_name, info_text, image_url = shop_data
-#         message_text = f'Уточняю. Вы выбрали салон: {shop_name}\n Информация о салоне: {info_text}'
-#         bot.send_message(call.message.chat.id, message_text, parse_mode='HTML')
-#         if image_url:
-#             bot.send_photo(call.message.chat.id, photo=image_url)
-#     else:
-#         bot.send_message(call.message.chat.id, text='Салон не найден в базе данных')
-
-
-# @bot.message_handler(func=lambda message: True)
-# def handle_text_message(message):
-#     try:
-#         user_message = message.text.strip()
-
-#         cursor.execute("SELECT id, name, info, image_url FROM shops WHERE name ILIKE %s", (user_message,))
-#         shop_data = cursor.fetchone()
-
-#         if shop_data:
-#             shop_id, shop_name, info_text, image_url = shop_data
-#             message_text = f'Уточняю. Вы выбрали салон: {shop_name}\nИнформация о салоне: {info_text}'
-#             bot.send_message(message.chat.id, message_text, parse_mode='HTML')
-
-#             if image_url:
-#                 bot.send_photo(message.chat.id, photo=image_url)
-#         else:
-#             bot.send_message(message.chat.id, text='Салон не найден в базе данных')
-
-#     except Exception as e:
-#         print(e)
-#         bot.send_message(message.chat.id, "Произошла ошибка при обработке вашего сообщения")
-
-
-# # REVIEW 
-# def review():
-#     keyboard = types.ReplyKeyboardMarkup(row_width=2)
-#     yes_button = types.KeyboardButton(text='Да', callback_data=f'review_yes')
-#     no_button = types.KeyboardButton(text='Нет', callback_data=f'review_no')
-#     keyboard.add(yes_button, no_button)
-
-
-# # REVIEW CALLBACK
-# @bot.callback_query_handler(lambda call: call.data.startwith('review_'))
-# def handle_review_query(call):
-#     pass    
-
-
-# OLD OLD OLD OLD OLD OLD
-# import telebot
-# from telebot import types
-
-# TOKEN = '6534971012:AAEXWo4l06RS8U-6nzXbrwmGVy5YnUZNBIs'
-
-# bot = telebot.TeleBot(TOKEN)
-
-# def webAppKeyboard():
-#     keyboard = types.ReplyKeyboardMarkup(row_width=1)
-#     webAppTest = types.WebAppInfo('https://web-app-telegram-site.vercel.app/')
-#     one_button = types.KeyboardButton(text='Конструктор и запись', web_app=webAppTest)
-#     keyboard.add(one_button)
-
-#     return keyboard
-
-# #/start
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#             # Отправляем гифку
-#         sticker_id = 'CAACAgIAAxkBAAEKxHNlVmqDS5OS1wg6tPWy-luHOluKjgAC3AUAAj-VzArxX-zMZBcVlzME'  # Замените на реальную ссылку на ваш стикер
-#         bot.send_sticker(message.chat.id, sticker_id)
-
-#         # Отправляем приветственное сообщение
-#         welcome_message = (
-#         "Добро пожаловать в IMEL!\n"
-#         "IMEL – система, разрабатываемая для студий маникюра и салонов красоты, позволяющая оптимизировать процесс создания дизайна ногтей,"
-#         "подготовки мастера к работе с отдельным клиентом, предоставляющая аналитику на основе выбранных дизайнов"
-#         "Создайте заранее запись, используя конструктор (кнопка в меню)\n"
-#         "Выберите учебник:"
-#         )
-
-#         bot.send_message(message.chat.id, welcome_message, reply_markup=webAppKeyboard())
-
-# @bot.message_handler(content_types="web_app_data") #получаем отправленные данные 
-# def answer(webAppMes):
-#    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
-#    bot.send_message(webAppMes.chat.id, f"Спасибо, запрос о записи отправлен менеджеру. Вы выбрали: {webAppMes.web_app_data.data}") 
-#    #отправляем сообщение в ответ на отправку данных из веб-приложения 
-
-# if __name__ == '__main__':
-#     bot.infinity_polling()
